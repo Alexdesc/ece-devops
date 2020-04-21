38c6b0d39df24fac1d77177fc8e5987d2d3dcd5c
Alex <alex@alex-omen-laptop.home>
2020-04-21 17:37:57 +0200

    controllers : user todo implementation

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,10 +6,19 @@ const os = require('os')
 const create = (user, callback) => {
 
   // Check users parameters
-  if(!user.username)
+  if(!user.username || !user.firstname || !user.lastname)
     return callback(new Error('Wrong users parameters'), null)
 
-  // TODO check if the user already exists using get()
+    // Check if user already exists
+    User.findOne({username: username}, function(err, data) {
+      if (err || data !== null) return callback(new Error('The username already exists'), null);
+      // Create an instance of model User and save it passing a callback
+      var newUser = new User({ username: username, firstname: firstname, lastname: lastname });
+      newUser.save(function (err) {
+        if (err) return callback(new Error(`Error while trying to save user ${username} in the database`), null);
+        callback(null, user.username);
+      });
+    });
 
   // Preparing a string with user parameters
   // const srtUser = user.username + ':' + user.firstname + ':' + user.lastname + os.EOL
@@ -25,12 +34,14 @@ const create = (user, callback) => {
     callback(null, user.username)
   })
 
-
 }
 
-// TODO create the get method
+// Get method to get a user by username
 const get = () => {
-
+  users.findOne({username: username}, function(err, data) {
+    if (err) return callback(new Error(`Error while trying to find user ${username} in the database`), null);
+    callback(null, data);
+  });
 }
 
 module.exports = {
