585f1140f1ddab957e32a30f9f3bfc600f201619
Alex <alex@alex-omen-laptop.home>
2020-04-25 13:02:00 +0200

    Implement get function to users

--- src/routes/users.js
+++ src/routes/users.js
@@ -16,8 +16,19 @@ usersRouter
     })
   })
   .get('/:username', (req, res, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
-    // TODO Create get method API
-    const username = req.params.username
+    const username = req.params.username;
+    users.get(username, (err, result) => {
+      if (err) return res.status(400).json({
+        error: {
+          message: err.message,
+        }
+      });
+      res.status(200).json({
+        username: result.username,
+        firstname: result.firstname,
+        lastname: result.lastname,
+      })
+    })
   })
 
 module.exports = usersRouter
